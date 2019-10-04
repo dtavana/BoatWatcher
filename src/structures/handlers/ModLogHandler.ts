802261648af9a3e2978dda4cfe65470d4d18ee48
Darian Tavana <datavana@gmail.com>
2019-10-04 17:10:01 -0400

    Fix pattern matching and log table name

--- src/structures/handlers/ModLogHandler.ts
+++ src/structures/handlers/ModLogHandler.ts
@@ -1,7 +1,7 @@
-import {Message, EmbedOptions} from 'eris';
+import {EmbedOptions, Message} from 'eris';
 import {BoatWatcherClient} from '../client';
-import {BaseHandler} from './BaseHandler';
 import { Context } from '../commands';
+import {BaseHandler} from './BaseHandler';
 
 class ModLogHandler extends BaseHandler {
     constructor(client: BoatWatcherClient) {
@@ -10,7 +10,7 @@ class ModLogHandler extends BaseHandler {
 
     public async run(message: Message): Promise<void> {
         const ctx = new Context(this.client, message, null);
-        if(ctx.guild.id !== this.client.config.DBL_GUILD_ID || message.channel.id !== this.client.config.DBL_MOD_LOG_ID) return;
+        if (ctx.guild.id !== this.client.config.DBL_GUILD_ID || message.channel.id !== this.client.config.DBL_MOD_LOG_ID) { return; }
 
         // const embed: EmbedOptions = message.embeds[0];
         const embed: EmbedOptions = {
@@ -18,37 +18,37 @@ class ModLogHandler extends BaseHandler {
             fields: [
                 {
                     name: 'Bot',
-                    value: 'The Standard#1912 (<@624360006472695841>)'
+                    value: 'The Standard#1912 (<@624360006472695841>)',
                 },
                 {
                     name: 'Moderator',
-                    value: 'Timo Halofan#7092'
+                    value: 'Timo Halofan#7092',
                 },
                 {
                     name: 'Reason',
-                    value: 'Your bot was offline when we tried to review it. For that reason, we are unable to test it. Please get your bot online and re-apply.'
+                    value: 'Your bot was offline when we tried to review it. For that reason, we are unable to test it. Please get your bot online and re-apply.',
                 },
 
-            ]
+            ],
         };
-        //TODO: Make more modular
-        if(!embed || !(embed.title === 'Bot declined')) return;
-        let res: any = {};
-        if (!embed.fields) return;
+        // TODO: Make more modular
+        if (!embed || !(embed.title === 'Bot declined')) { return; }
+        const res: any = {};
+        if (!embed.fields) { return; }
         for (const field of embed.fields) {
             const { name, value } = field;
-            if (!value || !name) continue;
+            if (!value || !name) { continue; }
             let trueVal: string = '';
             if (name === 'Bot') {
                 const matches = value.match(/(?:<@!?)+([0-9]+)>?/);
-                if (!matches) continue;
+                if (!matches) { continue; }
                 trueVal = matches[1];
             } else {
                 trueVal = value;
             }
             res[name] = trueVal;
         }
-        await this.client.pg.none("INSERT INTO decline-logs (botid, moderator, reason) VALUES ($1, $2, $3);", [res.Bot, res.Moderator, res.Reason]);
+        await this.client.pg.db.none('INSERT INTO declinelogs (botid, moderator, reason) VALUES ($1, $2, $3);', [res.Bot, res.Moderator, res.Reason]);
     }
 
 }
