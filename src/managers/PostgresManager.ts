ffe8045145949474beb9a821fda7c8008db6f776
Darian Tavana <datavana@gmail.com>
2019-10-04 17:10:19 -0400

    Call init function to create tables

--- src/managers/PostgresManager.ts
+++ src/managers/PostgresManager.ts
@@ -2,18 +2,20 @@ import pgPromise from 'pg-promise';
 import { BoatWatcherClient, IBoatWatcherClientConfig } from '../structures';
 
 class PostgresManager {
-    private _db: any;
+    public db: any;
     private _client: BoatWatcherClient;
     private _config: IBoatWatcherClientConfig;
     constructor(client: BoatWatcherClient) {
         this._client = client;
         this._config = this._client.config;
         const pgp = pgPromise();
-        this._db = pgp(this._config.PG_CONNECTION);
+        this.db = pgp(this._config.PG_CONNECTION);
         this._client.loggers.sendLog('Postgres DB Connected', 'console', 'database');
+        this.init().then();
     }
     public async init(): Promise<void> {
         // Initalize tables here
+        await this.db.none('CREATE TABLE IF NOT EXISTS declinelogs (botid varchar, moderator varchar, reason varchar, recorded TIMESTAMP DEFAULT NOW());')
         this._client.loggers.sendLog('Postgres Tables initialized', 'console', 'database');
     }
 
