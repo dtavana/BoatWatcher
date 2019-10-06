8837ec38788f308551b1472a860eeb3dba183e56
Darian Tavana <datavana@gmail.com>
2019-10-05 22:56:34 -0400

    Utilize the message embeds rather than the test embed

--- src/structures/handlers/ModLogHandler.ts
+++ src/structures/handlers/ModLogHandler.ts
@@ -10,28 +10,9 @@ class ModLogHandler extends BaseHandler {
 
     public async run(message: Message): Promise<void> {
         const ctx = new Context(this.client, message, null);
-        if (ctx.guild.id !== this.client.config.DBL_GUILD_ID || message.channel.id !== this.client.config.DBL_MOD_LOG_ID) { return; }
+        if (ctx.guild.id !== this.client.config.DBL_GUILD_ID || message.channel.id !== this.client.config.DBL_MOD_LOG_ID || message.embeds.length === 0) { return; }
 
-        // const embed: EmbedOptions = message.embeds[0];
-        const embed: EmbedOptions = {
-            title: 'Bot declined',
-            fields: [
-                {
-                    name: 'Bot',
-                    value: 'The Standard#1912 (<@624360006472695841>)',
-                },
-                {
-                    name: 'Moderator',
-                    value: 'Timo Halofan#7092',
-                },
-                {
-                    name: 'Reason',
-                    value: 'Your bot was offline when we tried to review it. For that reason, we are unable to test it. Please get your bot online and re-apply.',
-                },
-
-            ],
-        };
-        // TODO: Make more modular
+        const embed: EmbedOptions = message.embeds[0];
         if (!embed || !(embed.title === 'Bot declined')) { return; }
         const res: any = {};
         if (!embed.fields) { return; }
@@ -50,7 +31,6 @@ class ModLogHandler extends BaseHandler {
         }
         await this.client.pg.db.none('INSERT INTO declinelogs (botid, moderator, reason) VALUES ($1, $2, $3);', [res.Bot, res.Moderator, res.Reason]);
     }
-
 }
 
 export { ModLogHandler };
