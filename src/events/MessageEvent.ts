5ce9b86605ebc990a5cb79e2855ab428026ef701
Darian Tavana <datavana@gmail.com>
2019-10-06 00:13:43 -0400

    Remote bloated logging

--- src/events/MessageEvent.ts
+++ src/events/MessageEvent.ts
@@ -5,14 +5,7 @@ import {BoatWatcherClient} from '../structures';
 class MessageEvent extends BaseEvent {
     constructor(client: BoatWatcherClient) {
         super(client, 'messageCreate', async (message: Message) => {
-            if (message.author.bot) {
-                client.loggers.sendLog('Ignoring message, sent message was by a bot', 'console');
-                return;
-            } else {
-                client.loggers.sendLog(`Saw message: ${message.content}`, 'console');
-                client.loggers.sendLog('Emitting all message event handlers', 'console');
-                client.emit('handle-message', message);
-            }
+            client.emit('handle-message', message);
         });
     }
 }
