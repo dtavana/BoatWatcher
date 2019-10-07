65be18115aa6804d5be1c8e0c347467ec89df73d
Darian Tavana <datavana@gmail.com>
2019-10-06 20:13:19 -0400

    Add support for delete logs

--- src/structures/handlers/ModLogHandler.ts
+++ src/structures/handlers/ModLogHandler.ts
@@ -12,10 +12,23 @@ class ModLogHandler extends BaseHandler {
         const ctx = new Context(this.client, message, null);
         if (ctx.guild.id !== this.client.config.DBL_GUILD_ID || message.channel.id !== this.client.config.DBL_MOD_LOG_ID || message.embeds.length === 0) { return; }
         const embed: EmbedOptions = message.embeds[0];
-        if (!embed || !embed.author || embed.author.name !== 'Bot declined') { return; }
-        const res: any = {};
-        if (!embed.fields) { return; }
-        for (const field of embed.fields) {
+        if (!embed || !embed.author) {
+            return;
+        } else if (embed.author.name === 'Bot declined') {
+            await this.handleDecline(embed);
+        } else if (embed.author.name === 'Bot deleted') {
+            await this.handleDeleted(embed);
+        } else {
+            return;
+        }
+    }
+    private async parseFields(fields: Array<{ name?: string, value?: string, inline?: boolean }>) {
+        const res = {
+            Bot: undefined,
+            Moderator: undefined,
+            Reason: undefined,
+        };
+        for (const field of fields) {
             const { name, value } = field;
             if (!value || !name) { continue; }
             let trueVal: string = '';
@@ -28,7 +41,21 @@ class ModLogHandler extends BaseHandler {
             }
             res[name] = trueVal;
         }
+        return res;
+    }
+    private async handleDecline(embed: EmbedOptions) {
+        if (!embed.fields) { return; }
+        const res = await this.parseFields(embed.fields);
         await this.client.pg.db.none('INSERT INTO declinelogs (botid, moderator, reason) VALUES ($1, $2, $3);', [res.Bot, res.Moderator, res.Reason]);
+    }
+
+    private async handleDeleted(embed: EmbedOptions) {
+        if (!embed.fields) { return; }
+        const res = await this.parseFields(embed.fields);
+        const botDeletedByMod = await this.client.pg.db.oneOrNone('SELECT * FROM deletedlogs WHERE botid = $1 AND recorded + interval \'10 seconds\' > NOW();', res.Bot);
+        if (botDeletedByMod) {
+            await this.client.pg.db.none('UPDATE deletedlogs SET reason = $1 WHERE botid = $2 AND recordid = $3;', [res.Reason, res.Bot, botDeletedByMod.recordid]);
+        }
     }
 }
 
