6970c40a5f6a82c922ed16ada53629bd2104dce0
Darian Tavana <datavana@gmail.com>
2019-10-06 20:13:08 -0400

    Export LogHandler

--- src/structures/handlers/index.ts
+++ src/structures/handlers/index.ts
@@ -1,3 +1,4 @@
 export { BaseHandler } from './BaseHandler';
 export { CommandHandler } from './CommandHandler';
 export { ModLogHandler } from './ModLogHandler';
+export { LogHandler } from './LogHandler';
