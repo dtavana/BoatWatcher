2ebdd07ab85e4391be6408f95728dd74c2e7760e
Darian Tavana <datavana@gmail.com>
2019-10-07 11:41:47 -0400

    Finish route and controller

--- src/server/controllers/deleted.controller.ts
+++ src/server/controllers/deleted.controller.ts
@@ -1,5 +1,19 @@
 const getDeleted = async (req, res) => {
-    return;
+    const db = req.app.locals.db;
+    const client = req.app.locals.client;
+    const { botId } = req.body;
+    if (!botId) {
+        res.status(401).send({
+            success: false,
+            data: 'Not botId was provided',
+        });
+    } else {
+        const logs = await db.any('SELECT * FROM deletedlogs WHERE botid = $1;', botId);
+        res.status(200).send({
+            success: true,
+            data: logs,
+        });
+    }
 };
 
 export { getDeleted };
--- src/server/routes/api/getDeleted.ts
+++ src/server/routes/api/getDeleted.ts
@@ -2,6 +2,6 @@ import express from 'express';
 import {getDeleted} from '../../controllers/deleted.controller';
 
 const router = express.Router();
-router.get('/getDeleted', getDeleted);
+router.get('/', getDeleted);
 
 export default router;
